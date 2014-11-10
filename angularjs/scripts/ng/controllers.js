01664c747789f5d3667098a47a0023f5e8c734aa
jsonchou <jsonchou329@gmail.com>
2014-11-10 14:55:09 +0800

    angularjs submit
    Signed-off-by: jsonchou <jsonchou329@gmail.com>

---
+++ angularjs/scripts/ng/controllers.js
@@ -0,0 +1,73 @@
+﻿var phonecatApp = angular.module('phonecatApp', []);
+
+phonecatApp.controller('PhoneListCtrl', function ($scope) {
+    $scope.phones = [
+      {
+          'name': 'Nexus S',
+          'snippet': 'Fast just got faster with Nexus S.'
+      },
+      {
+          'name': 'Motorola XOOM™ with Wi-Fi',
+          'snippet': 'The Next, Next Generation tablet.'
+      },
+      {
+          'name': 'MOTOROLA XOOM™',
+          'snippet': 'The Next, Next Generation tablet.'
+      }
+    ];
+});
+
+
+//数组
+phonecatApp.controller('ArrayCtrl', function ($scope) {
+    
+});
+
+phonecatApp.controller('UzaiCtrl', function ($scope) {
+    $scope.members = [
+        {
+            name: 'json',
+            sex: '男'
+        }, {
+            name: 'axiao',
+            sex: '男'
+        }, {
+            name: 'maeo',
+            sex: '女'
+        }, {
+            name: 'maxo',
+            sex: '女'
+        }
+    ]
+
+    $scope.orderUzai = 'sex';
+});
+
+
+//injection
+phonecatApp.controller('UzaiInjectionCtrl', function ($scope, $http) {
+    //http://10.1.11.8:214/test/member.json angularjs 存在跨域问题。
+    $http.get('/angularjs/scripts/ng/member.json').success(function (data) {
+        $scope.members = data;
+    })
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
