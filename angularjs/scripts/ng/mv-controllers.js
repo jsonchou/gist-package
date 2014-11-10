3671f757f0324790d970705cf7568edfe3556c33
jsonchou <jsonchou329@gmail.com>
2014-11-10 18:36:57 +0800

    demo8
    Signed-off-by: jsonchou <jsonchou329@gmail.com>

---
+++ angularjs/scripts/ng/mv-controllers.js
@@ -0,0 +1,15 @@
+﻿var phonecatControllers = angular.module('phonecatControllers', []);
+
+phonecatControllers.controller('PhoneListCtrl', ['$scope', '$http',
+  function ($scope, $http) {
+      $http.get('/angularjs/scripts/ng/mv.json').success(function (data) {
+          $scope.phones = data;
+      });
+
+      $scope.orderProp = 'age';
+  }]);
+
+phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams',
+  function ($scope, $routeParams) {
+      $scope.phoneId = $routeParams.phoneId;
+  }]);
---
+++ angularjs/scripts/ng/mv.js
@@ -0,0 +1,21 @@
+﻿var phonecatApp = angular.module('phonecatApp', [
+'ngRoute',
+'phonecatControllers'
+]);
+
+phonecatApp.config(['$routeProvider',
+  function ($routeProvider) {
+      $routeProvider.
+        when('/list', {
+            templateUrl: '/angularjs/views/member-list.html',
+            controller: 'PhoneListCtrl'
+        }).
+        when('/id:phoneId', {
+            templateUrl: '/angularjs/views/member-detail.html',
+            controller: 'PhoneDetailCtrl'
+        }).
+        otherwise({
+            redirectTo: '/phones'
+        });
+  }]);
+
