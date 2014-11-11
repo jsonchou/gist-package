ff82b834eb2a2d8056cb530f3cee5567015d0ace
jsonchou <jsonchou329@gmail.com>
2014-11-11 17:35:22 +0800

    angularjs application
    Signed-off-by: jsonchou <jsonchou329@gmail.com>

---
+++ angularjs/scripts/dev/d1.js
@@ -0,0 +1,8 @@
+﻿var devApp = angular.module('devApp', ['devControllers']);
+
+var devControllers = angular.module('devControllers', []);
+
+devControllers.controller("devCtrl1", ["$scope", function ($scope) {
+    $scope.num = 1;
+    $scope.price = 19.95;
+}]);
---
+++ angularjs/scripts/dev/d5.js
@@ -0,0 +1,20 @@
+﻿var devApp = angular.module('devApp', ['devCtrl']);
+
+var devCtrl = angular.module('devCtrl', []);
+
+
+devCtrl.controller('GCtrl', ['$scope', function ($scope) {
+    $scope.name = 'grand-json'
+    $scope.papa = "haha";
+}]);
+
+devCtrl.controller('PCtrl', ['$scope', function ($scope) {
+    $scope.name = 'parent-json'
+}]);
+
+devCtrl.controller('CCtrl', ['$scope', function ($scope) {
+    $scope.name = 'child-json'
+}]);
+
+
+
