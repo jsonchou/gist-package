533f916399940c17c3dc7171145347da46c6bea9
jsonchou <527426267@qq.com>
2014-12-17 19:03:47 +0800

    fix mongodb database
    Signed-off-by: jsonchou <527426267@qq.com>

--- nodejs/n2/nodejs/routes/postMsg.js
+++ nodejs/n2/nodejs/routes/postMsg.js
@@ -1,47 +1,46 @@
 var express = require('express');
 var router = express.Router();
 
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017');
+
+//生成数据表原型
+var msgSchema = mongoose.Schema({
+    phone: String,
+    content: String,
+    email: String,
+    date: String
+}, { collection: 'msg' });
+
+//建表
+var msgModel = mongoose.model('msg', msgSchema);
+
 /* GET home page. */
 router.get('/', function(req, res) {
-    console.log(req);
 
     var phone=req.param('phone');
     var content=req.param('content');
-    var email=req.param('email');
-
-
-    var mongoose = require('mongoose');
-    //链接数据库
-    mongoose.connect('mongodb://localhost:27017/');
-
-    var kittySchema = mongoose.Schema({
-        phone: String,
-        content: String,
-        email: String
+    var email = req.param('email');
+    var date = (new Date().getFullYear()) + '-' + (new Date().getMonth() + 1) + '-' + (new Date().getDay());
+       
+    // yay!
+    var msgData = new msgModel({
+        phone: phone,
+        content: content,
+        email: email,
+        date:date
     });
-
-    //建表
-    var Kitten = mongoose.model('Kitten', kittySchema);
-
-    var db = mongoose.connection;
-    db.on('error', console.error.bind(console, 'connection error:'));
-    db.once('open', function callback () {
-        // yay!
-        var k1 = new Kitten({
-            phone:phone,
-            content:content,
-            email: email
-        });
-        debugger;
-        console.log(k1);
-        k1.save(err,function(){
-            if (err)
-                return console.error(err);
-        })
+     
+    msgData.save(function (err) {
+        if (err) {
+            console.log('##################');
+            console.log('error occur');
+            console.log('##################');
+        }
+       
+        res.render('postMsg', msgData);
     });
-
-    res.render('postMsg', { status:'your information has been insertd!'});
-
+     
 });
 
 module.exports = router;
