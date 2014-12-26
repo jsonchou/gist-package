5d036bbf16f17d00c87db9b201a503746d045439
jsonchou <527426267@qq.com>
2014-12-26 11:18:46 +0800

    add hi.js
    Signed-off-by: jsonchou <527426267@qq.com>

---
+++ nodejs/n3/routes/hi.js
@@ -0,0 +1,9 @@
+﻿var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function (req, res) {
+    res.render('hi', { title: '新手入门' });
+});
+
+module.exports = router;
