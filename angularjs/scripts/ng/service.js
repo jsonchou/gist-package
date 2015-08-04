ca96c57231a4d2633a6cc03620a0c8a856b7793b
jsonchou <527426267@qq.com>
2015-08-04 11:22:24 +0800

    empty angularjs folder
    Signed-off-by: jsonchou <527426267@qq.com>

--- angularjs/scripts/ng/service.js
+++
@@ -1,9 +0,0 @@
-﻿var phonecatServices = angular.module('phonecatServices', ['ngResource']);
-
-phonecatServices.factory('Mv', ['$resource',
-  function ($resource) {
-      ///angularjs/scripts/ng/mv.json
-      return $resource('/angularjs/scripts/ng/:pid.json', {}, {
-          query: { method: 'GET', params: { pid: 'list' }, isArray: true }
-      });
-  }]);
